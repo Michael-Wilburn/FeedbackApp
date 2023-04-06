72647d3276865ef79e565f1cddeef2c3d10bbfa7
Michael Wilburn <michael-mw@hotmail.com>
2023-04-06 15:06:37 -0300

    Creating feedbakApp with CreateReactApp

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,24 @@
+import {FaTimes} from 'react-icons/fa'
+import Card from '../components/shared/Card'
+import PropTypes from 'prop-types'
+
+export default function FeedbackItem({item, handleDelete}){
+    // const [rating, setRating] =  useState(7);
+    // const [text, setText] =  useState('');
+
+    
+
+    return(
+        <Card>
+            <div className="num-display">{item.rating}</div>
+            <button onClick={()=>{handleDelete(item.id)}} className="close">
+                <FaTimes color='purple'/>
+            </button>
+            <div className="tetx-display">{item.text}</div>
+        </Card>
+    )
+}
+
+FeedbackItem.propTypes = {
+    item: PropTypes.object.isRequired,
+}
