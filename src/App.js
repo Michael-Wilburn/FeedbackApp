8758e2e2529b95142a80ec810508801d68dd9b68
Michael Wilburn <michael-mw@hotmail.com>
2023-04-07 17:22:52 -0300

    AboutIconLink created, Links and routes set for client side navigation, small changes in index css

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import AboutIconLink from './components/AboutIconLink';
 import AboutPage from './pages/AboutPage';
 
 function App() {
@@ -25,25 +26,26 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Header />
-              <div className="container">
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
                 <FeedbackForm handleAdd={addFeedback} />
                 <FeedbackStats feedback={feedback} />
                 <FeedbackList
                   feedback={feedback}
                   handleDelete={deleteFeedback}
                 />
-              </div>
-            </>
-          }
-        />
-        <Route path="about" element={<AboutPage />} />
-      </Routes>
+                <AboutIconLink />
+              </>
+            }
+          ></Route>
+          <Route path="about" element={<AboutPage />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,5 @@
 import Card from "../components/shared/Card"
+import {Link} from 'react-router-dom'
 
 export default function AboutPage(){
     return(
@@ -7,6 +8,9 @@ export default function AboutPage(){
                 <h1>About This Project</h1>
                 <p>This is a React app to leave feedback fir a product or a service.</p>
                 <p>Version: 1.0.0</p>
+                <Link to='/'>
+                Back to Home
+                </Link>
             </div>
         </Card>
     )
